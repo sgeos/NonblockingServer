56c9aec946a68867a00b80d540d061991f49c98e
Brendan Alexander Raman Sechter <b.sechter@gloops.com>
2013-05-30 15:16:42 +0900

    Android, added comment block to ChatPreferenceActivity.

--- Android/src/com/gloops/androidchatclient/ChatPreferenceActivity.java
+++ Android/src/com/gloops/androidchatclient/ChatPreferenceActivity.java
@@ -1,3 +1,28 @@
+/***************************************************************
+ *
+ * ChatPreferenceActivity.java
+ * 
+ * This activity handles user preferences.
+ *
+ ***************************************************************
+ *
+ * This software was written in 2013 by the following author(s):
+ * Brendan A R Sechter <bsechter@sennue.com>
+ *
+ * To the extent possible under law, the author(s) have
+ * dedicated all copyright and related and neighboring rights
+ * to this software to the public domain worldwide. This
+ * software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain
+ * Dedication along with this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * Please release derivative works under the terms of the CC0
+ * Public Domain Dedication.
+ *
+ ***************************************************************/
+
 package com.gloops.androidchatclient;
 
 import android.os.Bundle;
